a57d3033ace14caca5d18374fa72f47a0b6c484c
r47dzt3ch <jeraldjose16@gmail.com>
2022-04-22 15:42:45 +0800

    updates

--- _admin/validate.php
+++ _admin/validate.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"]== "POST") {
 			echo "<script language='javascript'>";
 			echo "alert('WRONG INFORMATION')";
 			echo "</script>";
-			die();
+			print '<script>window.location.assign("index.php");</script>';//redirect to login.php	
 		}
 	}
 }
---
+++ fiveserver.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    highlight: true, // enable highlight feature
+    injectBody: true, // enable instant update
+    remoteLogs: true, // enable remoteLogs
+    remoteLogs: "yellow", // enable remoteLogs and use the color yellow
+    injectCss: false, // disable injecting css
+    navigate: true, // enable auto-navigation
+    // root: 'C:\\xampp\\htdocs\\cyberOps_NightWalker',
+    // php: 'C:\\xampp\\php\\php.exe' ,// Windows (example),
+    workspace=true
+}
